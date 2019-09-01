cb60872778a57459a8d139f3e17b8a0163cd5e59
ダルビッシュ <shoei0205@gmail.com>
2019-09-01 23:44:57 +0900

    2nd commit

--- index.js
+++ index.js
@@ -6,25 +6,9 @@ var UniversalBasicIncomeABI = [
 var UniversalBasicIncomeContractAddress = "0x9f2148d14aCB2284BE3F96C73E6C53c0aa33759d";
 
 function startApp() {
-  mary = new web3js.eth.Contract(UniversalBasicIncomeABI, UniversalBasicIncomeContractAddress);
+  UniversalBasicIncome = new web3js.eth.Contract(UniversalBasicIncomeABI, UniversalBasicIncomeContractAddress);
+  alert("Welcome UniversalBasicIncome");
 }
-
-function propose(address) {
-  // しばらく時間がかかるので、UIを更新してユーザーに
-  // トランザクションが送信されたことを知らせる
-  $("#txStatus").text("Proposing marriage on the block chain. This may take a while...");
-  // トランザクションをコントラクトに送信する:
-    return mary.methods.propose(address)
-    .send({ from: "0x40dea50302f41b7b695135b588b1ce2b5834ccd3" ,value: web3js.utils.toWei("0.001", "ether")})
-    .on("receipt", function(receipt) {
-      $("#txStatus").text("Successfully Proposed " + address + "!");
-      // トランザクションがブロックチェーンに取り込まれた。UIをアップデートしよう
-    })
-    .on("error", function(error) {
-      // トランザクションが失敗したことをユーザーに通知するために何かを行う
-      $("#txStatus").text(error);
-    });
-  }
 
 window.addEventListener('load', function() {
 
@@ -36,10 +20,9 @@ window.addEventListener('load', function() {
     // ユーザーがweb3を持たない場合の対処。
     // アプリを使用するためにMetamaskをインストールするよう
     // 伝えるメッセージを表示。
-    alert("hello ispec")
+    alert("please install Metamask")
   }
 
   // アプリのスタート＆Web3.jsへの自由なアクセスが可能に:
   startApp()
-
 })
